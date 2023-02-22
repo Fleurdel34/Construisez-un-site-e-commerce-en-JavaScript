c72b2d7080b1a59ee40c5cba6b004195d5a7658c
Fleurdel34 <betdevelweb0953@gmail.com>
2023-02-22 07:04:08 +0100

    insertion boucle dans condition

--- P5-Dev-Web-Kanap-master/front/js/product.js
+++ P5-Dev-Web-Kanap-master/front/js/product.js
@@ -73,9 +73,12 @@ boutonAjouterAuPanier.addEventListener("click", function(e){
         window.localStorage.setItem('kanapPanier', JSON.stringify(kanapPanier));
 
     }
-   else{            
+   else{  
+
         kanapPanier= JSON.parse(window.localStorage.getItem('kanapPanier'));
 
+        for(let kanap of kanapPanier){
+            
         const resultId = kanapPanier.find((kanap) => { 
             return kanap.id === objKanape.id;
         });
@@ -83,32 +86,15 @@ boutonAjouterAuPanier.addEventListener("click", function(e){
         const resultColors = kanapPanier.find((kanap) => { 
             return kanap.colors === objKanape.colors;
         });
-            
-        if(resultId === objKanape.id && resultColors === objKanape.colors)
-        {
-           const modifQuantitePanier = kanapPanier.map((kanap) =>{
-            
-                let resulPanier={
-                    'id': kanap.id,
-                    'colors': kanap.colors,
-                    'quantite': kanap.quantite
-                }
-            });
 
-            resulPanier.quantite = objKanape.quantite;
+            if(resultId === objKanape.id && resultColors === objKanape.colors){
+                kanap.quantite = objKanape.quantite;
+                window.localStorage.setItem('kanapPanier', JSON.stringify(kanapPanier));
+            }else{
 
-            const returnedObjPanier = Object.assign(objKanape, resulPanier);
-            
             kanapPanier.push(objKanape);
             window.localStorage.setItem('kanapPanier', JSON.stringify(kanapPanier));
-            
-
-        }else{
-
-        kanapPanier.push(objKanape);
-        window.localStorage.setItem('kanapPanier', JSON.stringify(kanapPanier));
-
+    
         }
-        
-    }
+   }
 });  
